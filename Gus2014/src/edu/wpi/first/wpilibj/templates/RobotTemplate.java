beb81af47adbb7ea897ef20f12b3af878de9ec17
Jacob Lint <guskid228@gmail.com>
2014-03-01 17:19:54 -0500

    Decent curve
    NEEDS THREAD EXCEPTIONS

--- Gus2014/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Gus2014/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,56 +1,70 @@
-
 package edu.wpi.first.wpilibj.templates;
 
-
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Dashboard;
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 
 public class RobotTemplate extends SimpleRobot {
+
     RobotDrive drive;
     Victor SL1, SL2, SR1, SR2, IntakeLeft, IntakeRight;
     Compressor comp;
     Shooter shooter;
-    DriverStation ds;
-    Dashboard DS;
-    
+    Joystick CoDriver, Left, Right;
+
     public void robotInit() {
         //Drivetrain
-        drive = new RobotDrive(1,2,3,4);
+        drive = new RobotDrive(1, 2, 3, 4);
         //Compressor
-        comp = new Compressor(8,1);
+        comp = new Compressor(8, 1);
         //Intake victors
         IntakeLeft = new Victor(5);
-        IntakeRight = new Victor(5);
+        IntakeRight = new Victor(6);
         //Start Compressor
         comp.start();
         //Shooter Thread
         shooter = new Shooter();
-        shooter.run();
-        DS.addInt(50);
+        Left = new Joystick(1);
+        Right = new Joystick(2);
+        CoDriver = new Joystick(3);
+        //  shooter.start();
+        ////////////////////////////////////
+        //TEST/////////////////////////////
+       // SL1 = new Victor(7);
+       // SL2 = new Victor(8);
+       // SR1 = new Victor(9);
+       // SR2 = new Victor(10);
+
     }
-    
-    
-    
+
     public void autonomous() {
-      
+
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-            shooter.Fire();
-            
+        while (isOperatorControl() && isEnabled()) {
+            drive.tankDrive(Left, Right);
+
+            if (CoDriver.getRawButton(1)) {
+                System.out.print("Pressed");
+                shooter.Fire();
+        
+            }
+
+        }
+
     }
-    
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
+
     }
 }
--- Gus2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
+++ Gus2014/src/edu/wpi/first/wpilibj/templates/Shooter.java
@@ -5,6 +5,7 @@
  */
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 
@@ -14,15 +15,49 @@ import edu.wpi.first.wpilibj.Victor;
  */
 public class Shooter extends Thread {
     //Shooter Victors
-    Victor SL1 = new Victor(5);
-    Victor SL2 = new Victor(5);
-    Victor SR1 = new Victor(5);
-    Victor SR2 = new Victor(5);
+    Joystick co = new Joystick(3);
+    Victor SL1 = new Victor(7);
+    Victor SL2 = new Victor(8);
+    Victor SR1 = new Victor(9);
+    Victor SR2 = new Victor(10);
 
 //Full speed fire
-    public void Fire() {
-        SL1.set(0.25);
-        Timer.delay(0.05);
+    public void run() {
+        
+        
+        
     }
-
+ public void Fire() {
+                
+                SL1.set(0.2);
+                SL2.set(0.2);
+                SR1.set(-0.2);
+                SR2.set(-0.2);
+                Timer.delay(0.025);
+                SL1.set(0.4);
+                SL2.set(0.4);
+                SR1.set(-0.4);
+                SR2.set(-0.4);
+                Timer.delay(0.015);
+                SL1.set(0.6);
+                SL2.set(0.6);
+                SR1.set(-0.6);
+                SR2.set(-0.6);
+                Timer.delay(0.010);
+                SL1.set(0.8);
+                SL2.set(0.8);
+                SR1.set(-0.8);
+                SR2.set(-0.8);
+                Timer.delay(0.005);
+                SL1.set(1.0);
+                SL2.set(1.0);
+                SR1.set(-1.0);
+                SR2.set(-1.0);
+                Timer.delay(0.4);
+                SL1.set(0.0);
+                SL2.set(0.0);
+                SR1.set(0.0);
+                SR2.set(0.0);
+                Timer.delay(1.0);
+    }
 }
